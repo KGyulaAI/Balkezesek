02056df89c5b2d21bfd2679135714a5fbb5e5640
Gyula <Gyula>
2023-05-25 11:48:56 +0200

    4. óra vége.

---
+++ Balkezesek/Balkezesek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    internal class Balkezesek
+    {
+        string nev;
+        string elso;
+        string utolso;
+        int suly;
+        int magassag;
+
+        public Balkezesek(string nev, string elso, string utolso, int suly, int magassag)
+        {
+            this.nev = nev;
+            this.elso = elso;
+            this.utolso = utolso;
+            this.suly = suly;
+            this.magassag = magassag;
+        }
+
+        public string Nev { get => nev; }
+        public string Elso { get => elso; }
+        public string Utolso { get => utolso; }
+        public int Suly { get => suly; }
+        public int Magassag { get => magassag; }
+    }
+}
--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Balkezesek> balkezesekLista = new List<Balkezesek>();
+
+            StreamReader streamReader = new StreamReader("balkezesek.csv");
+            streamReader.ReadLine();
+            while (!streamReader.EndOfStream)
+            {
+                string[] sor = streamReader.ReadLine().Split(";");
+                balkezesekLista.Add(new Balkezesek(sor[0], sor[1], sor[2], int.Parse(sor[3]), int.Parse(sor[4])));
+            }
+
+            Console.WriteLine($"2. feladat: {balkezesekLista.Count}");
+            Console.WriteLine();
         }
     }
 }
